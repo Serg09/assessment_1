36c1bf53829faad13bbeb6b6f95e9b84924e8b72
Serg09 <sergeyskumatov@aol.com>
2015-10-16 01:44:14 -0600

    Revert "Revert "Credit Card assessment_1""
    This reverts commit de018d4ade9bea83b5e812992da03eb3da47fcc5.
    
    [#105732274]

--- credit_card.rb
+++ credit_card.rb
@@ -1,11 +1,39 @@
-# Creadit Card
+# Version TWO
 class CreditCard
-  attr_reader :balance
-  def initialize
+  attr_reader :balance, :deposit, :withdraw
+
+  def initialize(_balance, deposit, withdraw)
     @balance = 0.00
+    @deposit = deposit
+    @withdraw = withdraw
   end
 
-  def charge(amount)
+  def deposit(amount)
     @balance += amount
   end
+
+  def withdraw(amount_2)
+    @balance -= amount_2
+  end
+end
+
+creditcard = CreditCard.new('balance', 'deposit', 'withdraw')
+
+puts "The account balance is $#{format '%.2f', (creditcard.balance)}"
+puts 'How much do you want to deposit?'
+amount = gets.chomp.to_f
+puts "You have deposited $#{format '%.2f', (creditcard.deposit(amount))}"
+puts "The new account balance is: $#{format '%.2f', (creditcard.balance)}"
+puts 'How much do you want to withdraw?'
+amount_2 = gets.chomp.to_f
+puts "The amount of your withdrawal is: $#{amount_2}"
+print 'The new account balance is: $'
+puts "#{format '%.2f', (creditcard.withdraw(amount_2))}"
+
+if creditcard.balance >= 25.00
+  puts 'Thank you for using a Bank.'
+elsif creditcard.balance < 0.00
+  puts 'ALERT: Negative balance!'
+else
+  puts 'WARNING: Low balance!'
 end
